82156756ad8d2fb234e824a416742b29fd1a095f
Sebastian Poweska <spoweska98@gmail.com>
2021-07-08 15:21:27 +0200

    Added Models controller+views

---
+++ LocalMotoAdsWebsite/LocalMotoAdsWebsite/Controllers/ModelsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LocalMotoAdsWebsite.Data;
+using LocalMotoAdsWebsite.Models;
+using LocalMotoAdsWebsite.Models.ViewModels;
+
+namespace LocalMotoAdsWebsite.Controllers
+{
+    public class ModelsController : Controller
+    {
+        private readonly AppDbContext _context;
+        [BindProperty]
+        public ModelViewModel ModelVM { get; set; }
+
+
+        public ModelsController(AppDbContext context)
+        {
+            _context = context;
+            ModelVM = new ModelViewModel()
+            {
+                Makes = _context.Makes.ToList(),
+                Model = new Models.Model()
+            };
+        }
+
+        // GET: Models
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Models.Include(m => m.Make);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Models/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Models
+                .Include(m => m.Make)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        // GET: Models/Create
+        public IActionResult Create()
+        {
+            ViewData["MakeFK"] = new SelectList(_context.Makes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Models/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,MakeFK")] Model model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MakeFK"] = new SelectList(_context.Makes, "Id", "Name", model.MakeFK);
+            return View(model);
+        }
+
+        // GET: Models/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Models.FindAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            ViewData["MakeFK"] = new SelectList(_context.Makes, "Id", "Name", model.MakeFK);
+            return View(model);
+        }
+
+        // POST: Models/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,MakeFK")] Model model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModelExists(model.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MakeFK"] = new SelectList(_context.Makes, "Id", "Name", model.MakeFK);
+            return View(model);
+        }
+
+        // GET: Models/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Models
+                .Include(m => m.Make)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        // POST: Models/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var model = await _context.Models.FindAsync(id);
+            _context.Models.Remove(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModelExists(int id)
+        {
+            return _context.Models.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ LocalMotoAdsWebsite/LocalMotoAdsWebsite/Models/ViewModels/ModelViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocalMotoAdsWebsite.Models.ViewModels
+{
+    public class ModelViewModel
+    {
+        public Model Model { get; set; }
+        public IEnumerable<Make> Makes { get; set; }
+        //public IEnumerable<SelectListItem> CSelectListItems(IEnumerable<Make> Items)
+        //{
+        //    List
+        //}
+    }
+}
